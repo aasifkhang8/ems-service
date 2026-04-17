c5c69be893d01eae7c2c006ac957f45ba8253a0e
aasif khan <aasifkhang8@gmail.com>
2026-04-17 13:02:04 +0530

    initial commit

---
+++ src/test/java/com/org/ems_service/EmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.ems_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
